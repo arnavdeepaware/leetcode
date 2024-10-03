1127fd0587ef8acea8e8c12def878b1c9b3859b3
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2024-10-03 17:35:49 -0400

    Time: 0 ms (100%), Space: 42.5 MB (61.04%) - LeetHub

---
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        
+        int runningSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++){
+            runningSum += nums[i];
+            nums[i] = runningSum;
+        }
+
+    return nums;
+    }
+}
