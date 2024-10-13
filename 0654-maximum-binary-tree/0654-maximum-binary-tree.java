dfec613af563e058cbf2c37b2bfb06528433eab9
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2024-10-12 22:09:36 -0400

    Time: 1 ms (100%), Space: 44.7 MB (67.65%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.java
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.java
@@ -1,40 +1,41 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        return helper(nums,0,nums.length-1);
+        return helper(nums, 0, nums.length-1);
     }
 
-    private TreeNode helper(int[] nums, int l, int r) {
-        if (l > r) {
+    public TreeNode helper(int[] nums, int l, int r){
+        if(l > r){
             return null;
         }
-        int max = Integer.MIN_VALUE;
-        int mid = -1;
-        for (int i = l; i <= r; i++) {
-            if (nums[i] > max) {
-                max = nums[i];
-                mid = i;
-            }
 
+        int max = nums[l];
+        int maxIndex = l;
+
+        for(int i = l+1; i <= r; i++){
+            if(nums[i] > max){
+                max = nums[i];
+                maxIndex = i;
+            }
         }
 
         TreeNode root = new TreeNode(max);
-        root.left = helper(nums, l, mid - 1);
-        root.right = helper(nums, mid + 1, r);
+        root.left  = helper(nums, l, maxIndex - 1);
+        root.right = helper(nums, maxIndex + 1, r);
 
         return root;
     }
