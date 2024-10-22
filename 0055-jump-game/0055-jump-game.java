52c5c9749cd6caf8734475e6152ac023ee1f7b6b
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2024-10-22 14:18:18 -0400

    Time: 3 ms (49.76%), Space: 46.1 MB (9.51%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,34 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        /*
-            - Create a set
-            - The set will contain the indexes that cen be possible visited
-            - At each index, we add possible indexes to the set
-            - We traverse the array
-            - If the index is not possible, we skip it
-            - At the end of the traversal, last inddex value should be in our set
-        */
 
-        HashSet<Integer> set = new HashSet<>();
+        int max = nums[0]; 
 
-        if(nums.length < 2){
-            return true;
+        for(int i = 1; i < nums.length - 1; i++){
+
+            if( i > max){
+                return false;
+            }
+            int jumpPos = i + nums[i];
+
+            max = Math.max(max, jumpPos);
         }
 
-        set.add(0);
-
-        for(int i = 0; i < nums.length - 1; i++){
-            if(set.contains(i)){
-                for(int j = 1; j <= nums[i]; j++){
-                    set.add( i + j);
-                }
-            }
-
-            if(set.contains(nums.length - 1)){
-                return true;
-            }
-        }
-
-        return false;
+        return max >= nums.length - 1;
+        
     }
 }
