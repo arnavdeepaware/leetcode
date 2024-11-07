49b41de4f5d6233d4faa4bd57cc1ad9476408eed
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2024-11-07 16:35:55 -0500

    Time: 0 ms (100%), Space: 40 MB (89.67%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fib(int n) {
+
+        if(n < 2){
+            return n;
+        }
+
+        int[] nums = new int[n+1];
+        nums[1] = 1;
+
+        for(int i=2; i <= n; i++){
+            nums[i] = nums[i-1] + nums[i-2];
+        }
+
+        return nums[n];
+        
+    }
+}
