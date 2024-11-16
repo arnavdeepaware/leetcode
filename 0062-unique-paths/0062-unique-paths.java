83ecce0f60c1d3e7a1d7ff4a99c3cd07d4626a3a
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2024-11-16 10:45:43 -0500

    Time: 0 ms (100%), Space: 40.7 MB (19.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,31 +1,19 @@
 class Solution {
-public int uniquePaths(int i, int j) {
-	
-	//Edge Case
-	// if(i == 1 || j == 1){
-	// 	return 1;
-    // }
+    public int uniquePaths(int m, int n) {
 
-    int[][] paths = new int[i][j];
+        int[][] paths = new int[m][n];
 
-    return helper(i-1, j-1, paths);
-}
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if( i == 0 || j == 0){
+                    paths[i][j] = 1;
+                }else{
+                    paths[i][j] = paths[i-1][j] + paths[i][j-1];
+                }
+            }
+        }
 
-public int helper(int i, int j, int[][] paths){
-	
-//Base Case
-if(i == 0 || j == 0){
-		paths[i][j] = 1;
-return 1;
-}
+        return paths[m-1][n-1];
 
-//Memoization
-if(paths[i][j] > 0){
-	return paths[i][j];
-}
-
-paths[i][j] = helper(i-1, j, paths) + helper(i, j-1, paths);
-return paths[i][j];
-}
-
+    }
 }
