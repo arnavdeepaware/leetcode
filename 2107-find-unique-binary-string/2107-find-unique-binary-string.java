dfa37e534885d83151b4c71e3c1a8d307844dffe
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2024-11-21 01:32:31 -0500

    Time: 92 ms (20.73%), Space: 45.3 MB (10.5%) - LeetHub

---
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        /*
+            - Range of possible nums will be [0, 2^n)
+            - Max 16 nums
+            - Convert each from string-binary to int and store into a set
+            - Loop from 2^n-1 to 0
+            - find missing number and convert to string-binary
+            - Return missing number
+        */
+
+        HashSet<Integer> set = new HashSet<Integer>();
+
+        int n = nums.length;
+        String ans = "0";
+
+        for(int i = 0; i < nums.length; i++){
+            int num = Integer.parseInt(nums[i], 2);
+            set.add(num);
+        }
+
+        for(int i = (int) Math.pow(2, n) - 1; i >= 0; i--){
+            if(set.contains(i)){
+                continue;
+            }else{
+                ans = Integer.toBinaryString(i);
+            }
+        }
+
+        while(ans.length() < n){
+            ans = "0" + ans;
+        }
+
+        return ans;
+    }
+}
