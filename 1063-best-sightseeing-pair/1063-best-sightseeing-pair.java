c4e45a8e9b493488a7087ac5e9736ce9a590e055
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2024-12-26 23:09:26 -0500

    Time: 4 ms (89.46%), Space: 54 MB (9.46%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
@@ -1,5 +1,17 @@
 class Solution {
     public int maxScoreSightseeingPair(int[] values) {
+        /*
+            - Approach
+                - We store our result and max value on the left side
+                - With each iteration we check if our max + current[j] > max
+                    - If yes we update our res
+                - At the end of each iteration, our max holds the max element traversed - distance
+                - At start of each iteration, we decrement max to account for the subtracting the distance
+            
+            - Logic:
+                - We alwways store the highest possible element - distance as it will yield the highest value from the left side of the array
+                - As at each iteration we are calculating the max, we need to just check for the highest result based on current iteration
+        */
         int result = 0;
         int max = values[0];
 
