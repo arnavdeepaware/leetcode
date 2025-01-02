c8d5069f2b5c5ed9edfaa40db04ead5a5e706259
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-01-02 18:03:03 -0500

    Time: 2 ms (97.44%), Space: 44.9 MB (45.91%) - LeetHub

--- 3226-minimum-number-game/3226-minimum-number-game.java
+++ 3226-minimum-number-game/3226-minimum-number-game.java
@@ -1,27 +1,14 @@
 class Solution {
     public int[] numberGame(int[] nums) {
         
-        //Sorting Array
-        for (int i = 0; i < nums.length; i++){
-            int min = nums[i];
-            int minIndex = i;
-            for (int j = i+1; j < nums.length; j++){
-                if (nums[j] < min){
-                    min = nums[j];
-                    minIndex = j;
-                }
-            }
-            nums[minIndex] = nums[i];
-            nums[i] = min;
-            
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i += 2){
+            //Swap i and i+1 element
+            int temp = nums[i];
+            nums[i] = nums[i+1];
+            nums[i+1] = temp;
         }
 
-        for ( int i = 0; i < nums.length; i += 2){
-                int temp = nums[i];
-                nums[i] = nums[i+1];
-                nums[i+1] = temp;
-            }
-            
         return nums;
     }
 }
