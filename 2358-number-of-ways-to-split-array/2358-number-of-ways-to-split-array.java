79010ae6492c8e0c3172cc18b6c4a5f093d2cb24
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-01-03 12:12:48 -0500

    Time: 10 ms (2.92%), Space: 58.6 MB (55.4%) - LeetHub

---
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        //Sum the entire array
+        // For every i - split the sum into left and right
+        // Add ith element to left and remove it from right
+        //Compare left and right and increment count accordingly
+        //Traverse from index 0 to n - 1
+        //Return count
+
+        
+        // Sum of the array
+        long sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum += (long) nums[i];
+        }
+
+        System.out.println("Total sum: " + sum);
+
+        //Find valid splits
+        long leftSum = 0;
+        long rightSum = sum;
+        int count = 0;
+
+        for(int i = 0; i < nums.length - 1; i++){
+            leftSum += (long)nums[i];
+            rightSum -= (long)nums[i];
+
+            //Check for split
+            if(leftSum >= rightSum){
+                count ++;
+            }
+        }
+
+        return count;
+    }
+}
