86b2707ee97a4c371e06325a3e7666f2ef16b5b8
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-01-04 13:00:16 -0500

    Time: 0 ms (100%), Space: 42 MB (49.09%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,45 +1,20 @@
 class Solution {
     public int findMin(int[] nums) {
-        /*
-            - Binary Search
-            - 4 Cases
-            - Every rotate has an inflection point where the min element is either first element or min is right after the max element
-            - For mid
-                - if mid + 1 is smaller: return mid + 1
-                - if mid - 1 is smaller: return mid - 1; check edge case
-            - If mid is smaller than last: check left half
-            - else check rigth half        
-         */
+        
+        int lo = 0;
+        int hi = nums.length - 1;
 
-         int left = 0;
-         int right = nums.length - 1;
+        while(lo < hi){
+            int mid = (lo + hi) / 2;
+            int num = nums[mid];
 
-         while(left <= right){
-
-            if(left == right){
-                return nums[left];
-            }
-            
-            int mid = (left + right) / 2;
-            int midNum = nums[mid];
-            System.out.println(midNum);
-
-            if(nums[mid + 1] < midNum){
-                return nums[mid + 1];
-            }
-
-            if(mid > 0 && nums[mid - 1] > midNum){
-                return midNum;
-            }
-
-            if(midNum < nums[right]){
-                right = mid - 1;
-                // System.out.println("right: " + right);
+            if(num > nums[hi]){
+                lo = mid + 1;
             }else{
-                left = mid + 1;
+                hi = mid;
             }
         }
 
-        return nums[0];
+        return nums[lo];
     }
 }
