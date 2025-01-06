fc19eb05a253d0b3e1ad9cbb448c24b6c162e260
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-01-06 14:12:30 -0500

    Time: 4 ms (76.56%), Space: 44.8 MB (46.88%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -9,29 +9,26 @@ class Solution {
         //Left side and Prefix
         int[] left = new int[n];
         int prefix = Character.getNumericValue(boxes.charAt(0));
-        int prev = 0;
+        left[0] = 0;
 
         for(int i=1; i<n; i++){
-            left[i] = prev + prefix;
+            left[i] = left[i-1] + prefix;
             prefix += Character.getNumericValue(boxes.charAt(i));
-            prev = left[i];
         }   
 
         //Right side and suffix
-        // int[] right = new int[n];
+        int[] right = new int[n];
         int suffix = Character.getNumericValue(boxes.charAt(n-1));
-        prev = 0;
+        right[n-1] = 0;
 
         for(int i = n-2; i >= 0; i--){
-            int curr = prev + suffix;
-            left[i] += curr;
+            right[i] = right[i+1] + suffix;
             suffix += Character.getNumericValue(boxes.charAt(i));
-            prev = curr;
         }
 
-        // for(int i=0; i <n; i++){
-        //     left[i] += right[i];
-        // }
+        for(int i=0; i <n; i++){
+            left[i] += right[i];
+        }
 
         return left;
     }
