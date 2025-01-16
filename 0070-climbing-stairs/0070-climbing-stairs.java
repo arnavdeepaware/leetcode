f145822bda0a67bffe4b5290a98a38c21a163fcf
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-01-16 00:53:28 -0500

    Time: 0 ms (100%), Space: 40.1 MB (87.19%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        if(n < 3){
+            return n;
+        }
+        
+        int[] steps = new int[n];
+        steps[0] = 1;
+        steps[1] = 2;
+
+        for(int i = 2; i < n; i++){
+            steps[i] += steps[i-2];
+            steps[i] += steps[i-1];
+        }
+
+        return steps[n-1];
+    }
+}
