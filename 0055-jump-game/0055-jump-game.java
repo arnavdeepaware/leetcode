d9c7bc139d34c97ac8fb7d7c70359d2251a3bb74
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-01-27 11:32:41 -0500

    Time: 3 ms (45.77%), Space: 45.8 MB (55.16%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,51 +1,16 @@
 class Solution {
     public boolean canJump(int[] nums) {
         
-        /*
-            - DP Approach
-            - Traverse from back to front of array
-            - At each iteration, check if target (indexes not values) is attainable
-                - If yes, the current element is now the target
-                - If not, continue
-            - After loop termination, target should be index 0
-                - If yes, we can reach the last index from the start: return True
-                - If not, we cannot reach last index: return False;
+        int max = 0;
 
-            - Edge Cases
-                - If nums.len == 1: return true
-                - If first element is zero: return false
-        */
-
-        //Edge Cases
-
-        //Array with single element
-        if(nums.length == 1){
-            return true;
-        }
-
-        //Array with starting element zero
-        if(nums[0] == 0){
-            return false;
-        }
-
-        //Starting target is last element
-        int targetIndex = nums.length - 1;
-
-        //Loop from back to start
-        for(int i = nums.length - 2; i >= 0; i--){
-            
-            //Check if target index is attainable
-            if(targetIndex <= i + nums[i]){
-                targetIndex = i;
+        for(int i = 0; i < nums.length - 1; i++){
+            if(i > max){
+                return false;
             }
+
+            max = Math.max(max, i + nums[i]);
         }
 
-        //Check if target index is zero
-        if(targetIndex == 0){
-            return true;
-        }
-        //Else
-        return false;
-
+    return max >= nums.length - 1;
     }
 }
