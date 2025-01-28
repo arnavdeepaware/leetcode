3cb48b01315e01cad40415ebd62486d2f35656ce
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-01-28 15:18:10 -0500

    Time: 3 ms (74.02%), Space: 43.1 MB (75.95%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,30 +1,45 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        /*
+            - Use a two pointer approach
+            - Convert to lowercase
+            - skip non-valid characters
+            - if current valid pointers do not match, return false
+            - at the end, return true
+        */
 
         s = s.toLowerCase();
 
-        int l = 0;
-        int r = s.length() - 1;
+        int left = 0;
+        int right = s.length() - 1;
 
-        while(l < r){
-            if(!Character.isLetterOrDigit(s.charAt(l))){
-                l++;
+        while(left < right){
+
+            boolean cont = false;
+
+            if(!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+                cont = true;
+            }
+
+            if(!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+                cont = true;
+            }
+
+            if(cont){
                 continue;
             }
 
-            if(!Character.isLetterOrDigit(s.charAt(r))){
-                r--;
-                continue;
-            }
-
-            if(s.charAt(l) != s.charAt(r)){
+            if(s.charAt(left) != s.charAt(right)){
                 return false;
-            }        
+            }
 
-            l++;
-            r--;    
-
+            left++;
+            right--;
         }
+
+
 
         return true;
     }
