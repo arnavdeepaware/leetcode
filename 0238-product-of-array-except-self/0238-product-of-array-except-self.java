77c58fe6470df9006c78ccde63acc3e0dfcb0956
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-01-30 07:37:53 -0500

    Time: 2 ms (88.49%), Space: 55.6 MB (51.55%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,30 +1,16 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        /*
-            - Two Pointer Approach
-            - Keep track of prefix and suffix products
-            - Add and multiply them in the ans array
-            - prefix is for i+1
-            - suffix is for i -1
-            - By default the prefix of 0th is 1
-            - By default the suffix of last element is 1
-        */
 
-        // Output Array 
-        int n = nums.length;
-        int[] ans = new int[n];
+        int[] ans = new int[nums.length];
 
-
-        //prefix
         int pre = 1;
-        for(int i = 0; i < n; i++){
+        for(int i = 0 ; i < nums.length; i++){
             ans[i] = pre;
             pre *= nums[i];
-        }
+        }        
 
-        //suffix
         int suf = 1;
-        for(int i = n-1; i >= 0; i--){
+        for(int i = nums.length-1; i>=0; i--){
             ans[i] *= suf;
             suf *= nums[i];
         }
