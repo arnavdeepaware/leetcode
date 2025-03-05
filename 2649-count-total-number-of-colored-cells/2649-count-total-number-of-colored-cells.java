3f32568e614fd5fff88315e74921a0f5350b67bb
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-03-05 18:15:50 -0500

    Time: 0 ms (100%), Space: 40.6 MB (36.51%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -1,20 +1,7 @@
 class Solution {
     public long coloredCells(int n) {
 
-        long prev = 1;
-        long curr = 1;
-        long prevSum = 1;
-        long total = 1;
-
-        for(int i = 2; i <= n; i++){
-            
-            curr += 2;
-            total = 2 * prevSum + curr;
-            prev = curr;
-            prevSum += prev;
-        }
-        
-        return total;
+        return 1L + 2L * (n-1) * n;
         
     }
 }
@@ -25,3 +12,11 @@ class Solution {
 // 1       1       1         1
 // 1.      2.      prev+2    2 * prevSum + (curr) = 5
 // 3       3       prev+2    
+
+// n =5
+
+// 1       1
+// 3       4 (+3)
+// 5       13 (+11)
+// 7       25 (+12)
+// 9       41 (+16)
