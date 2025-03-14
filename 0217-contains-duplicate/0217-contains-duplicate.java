0f3f8b60750cc5acc66057b113a397b7165ee55c
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-03-13 22:32:15 -0400

    Time: 10 ms (88.65%), Space: 58.4 MB (32.13%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,26 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
 
-        //Storage Efficient
-        Arrays.sort(nums);
+        // //Storage Efficient
+        // Arrays.sort(nums);
 
-        for(int i = 0; i < nums.length - 1; i++){
-            if(nums[i] == nums[i+1]){
+        // for(int i = 0; i < nums.length - 1; i++){
+        //     if(nums[i] == nums[i+1]){
+        //         return true;
+        //     }
+        // }
+
+        // return false;
+
+        //Memory Efficient
+        HashSet <Integer> set = new HashSet<Integer>();
+
+        for(int n: nums){
+            if(set.contains(n)){
                 return true;
             }
+
+            set.add(n);
         }
 
         return false;
