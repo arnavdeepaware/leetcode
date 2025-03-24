96d9f72be697564ed087bd7301b94c4f77d72239
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-03-24 11:06:10 -0400

    Time: 5 ms (97.33%), Space: 46.1 MB (61.85%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -1,21 +1,33 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        
+
         /*
-            - Sort with start times
+            - Sort array - start time
+            - Empty input?
+            - Single interval?
+            - Repeated intervals
+            - End time = 2, next start time = 2
+
+            At each iteration
+                - Check if start time is valid based on previous end time
+                Ex:
+                    1. 2,4    5,6   --> True
+                    2. 2, 5   4, 38 --> False
+                    3. 1, 6   6, 9  --> True
         */
 
-        Arrays.sort(intervals, (a,b)->(a[0] - b[0]));
+        //Sort intervals
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
 
+        //Traversal
         for(int i = 1; i < intervals.length; i++){
-            //Stop if start time is not after prev end time
             if(intervals[i][0] < intervals[i-1][1]){
                 return false;
             }
         }
 
         return true;
-
         
+
     }
 }
