cc499e125009e8370165f2691171de6338408eaa
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-04-10 21:07:58 -0400

    Time: 0 ms (100%), Space: 45.2 MB (84.69%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,16 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
         
-        int targetSum = 0;
-        int actualSum = 0;
+        int res = nums.length;
 
-        for(int i=0; i<nums.length; i++){
-            targetSum += i;
-            actualSum += nums[i];
+        for(int i = 0; i < nums.length; i++){
+            res ^= i ^ nums[i];
         }
 
-        targetSum += nums.length;
-
-        return targetSum - actualSum;
+        return res;
     }
 }
