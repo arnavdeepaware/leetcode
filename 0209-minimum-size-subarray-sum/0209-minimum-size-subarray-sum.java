0233b638e73ef4ae5d4954feb46c6a648c1ec30a
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-05-21 15:55:13 -0400

    Time: 1 ms (99.78%), Space: 58.1 MB (53.3%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,25 +1,51 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
+        //Sliding Window Solution
 
+        //Two Pointers
+        int right = 0, left = 0;
+
+        int minLength = Integer.MAX_VALUE; // Returning value
         int sum = 0;
-        int min = Integer.MAX_VALUE;
 
-        int l=0;
-        int r=0;
+        while(right < nums.length){
+            sum += nums[right];
 
-        for(r=0; r < nums.length; r++){
-            if(nums[r] == target){
-                return 1;
+            while(left <= right && sum >= target){
+                minLength = Math.min(minLength, right - left + 1);
+                sum -= nums[left];
+                left++;
             }
 
-            sum += nums[r];
-
-            while(sum >= target){
-                min = Math.min(min, r - l + 1);
-                sum -= nums[l++];
-            }   
+            right ++;
         }
 
-        return min == Integer.MAX_VALUE? 0: min;
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+
     }
 }
+
+/*
+    - We are given an array
+    - Need to find LENGTH of subarray; min = 1
+    - s.t. sum of subarray >= target
+
+    - Empty array? -> atleast 1 element
+    - Is it sorted? -> no
+    - Negative numbers? -> positive
+    - If target dne, return 0
+
+    - Two pointers and sliding window approach
+    - two pointers - both at start
+    - Default sum is first element
+    - If sum is less < target -- move right by one and add the new val to sum
+    - else if sum >= target...
+        - check for min length 
+        - remove left pointer val from sum
+        - move left pointer by one
+    - end the loop when right exceeds the last element in nums
+
+    - case when left and right are at the same point and sum >= target:
+        - return 1 or when lenght of valid subarray is 1 .. return it
+*/
+
