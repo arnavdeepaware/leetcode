573718cd61c58c497690b3decf9c636d93c7e5aa
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-05-31 16:47:47 -0400

    Time: 3 ms (41.08%), Space: 42.1 MB (12.19%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,54 @@
 class Solution {
+    /*
+        - String be empty?
+    
+        - Use stack to keep order of parentheses
+        - Use hashmap to pair opening and closing types
+        - traverse the input
+        - if opening bracket, add to stack
+        - if not, i.e. closing bracket:
+                - check if stack is not empty (shouldn't be to be valid)
+                - it should be of same type as top element of stack to be valid
+                - if valid, pop the top element, continue
+                - else not valid, return false
+                
+        - At the end, check if stack is empty
+        - If yes, we have a valid input, return true
+        - else return false
+    */
+
+    
     public boolean isValid(String s) {
         
-        Stack<Character> st = new Stack<>();
+        //Hashmap
         HashMap<Character, Character> map = new HashMap<>();
-
         map.put(')', '(');
+        map.put(']', '[');
         map.put('}', '{');
-        map.put(']','[');
 
+        //Stack
+        Stack<Character> st = new Stack<>();
+
+        //Traversal
         for(char c: s.toCharArray()){
-            //If char is opening tag
+            
+            //Check for opening bracket
             if(!map.containsKey(c)){
-                st.push(c);
+                st.add(c);
+                
             }else{
-                if(st.empty() || st.pop() != map.get(c)){
+                if(st.empty() || map.get(c) != st.pop()){
                     return false;
                 }
             }
         }
 
-        return st.empty();
+        if(!st.empty()){
+            return false;
+        }
+
+        return true;
         
     }
 }
+
