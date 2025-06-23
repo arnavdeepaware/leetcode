29aba87abc4d10b6ceb2827f08a4aaef6f796d84
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-06-23 12:43:59 -0400

    Time: 1 ms (99.84%), Space: 52.9 MB (65.8%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,37 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2 ];
+        
+        // Boyer-Moore Voting Algorithm
+        /*
+            - We use increment and decrements
+            - We store the count and assume the first element as majority element
+            - Traverse the array, 
+                - If element is == majority, increment count
+                - Else, decrement 
+                - If anytime, count == 0, we reset the majority to next element
+            - Given the constraints of the problem, there are more majority elements than minority.
+            - We are cancelling the majority count by minority times.
+            - At the end, when all minorities are counted, we will inevitably be left with the majority
+            - It is important to never let count go to -1
+        */
+
+        int ct = 1;
+        int majority = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == majority){
+                ct++;
+            }else{
+                ct--;
+            }
+
+            if(ct == 0){
+                ct = 1;
+                majority = nums[++i];
+            }
+        }
+
+        return majority; 
+
     }
 }
