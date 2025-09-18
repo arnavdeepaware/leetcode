e11d902fc1df8a4f53d2aca7ca3e325cb0fb9823
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-09-18 12:06:59 -0400

    Time: 0 ms (100%), Space: 40.4 MB (77.32%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.java
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.java
@@ -1,19 +1,30 @@
-import java.util.*;
-
 class Solution {
     public boolean canMeasureWater(int x, int y, int target) {
-        if (x + y < target) {
+        
+        /*
+            - target is only achievable if target is a multiple of the gcd (a,b)
+            - gcd(a,b) is essentially the smallest possible am + bn
+            - given the actions, filling, emptying, or transferring water between jugs, we can simple say that the combinations will be part of am + an
+            - hence we need to return target % gcd(a,b) == 0
+        */
+
+        //Edge case
+        if(x+y < target){
             return false;
         }
-        
-        return target % gcd(x, y) == 0;
+
+        int gcd = findGcd(x, y);
+
+        return target % gcd == 0;
     }
-    private int gcd(int a, int b) {
-        while (b != 0) {
+
+    public int findGcd(int a, int b){   // Pre: x and y are positive
+        while(b != 0){
             int temp = b;
             b = a % b;
             a = temp;
         }
+
         return a;
     }
 }
