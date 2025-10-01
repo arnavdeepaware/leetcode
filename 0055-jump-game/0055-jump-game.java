1a251f3234faefa7b3acbd0e257e7948e16ad237
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2025-10-01 01:32:51 -0400

    Time: 2 ms (84.6%), Space: 45.6 MB (71.99%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        
-        int max = 0;
 
-        for(int i = 0; i < nums.length - 1; i++){
-            if(i > max){
+        int maxIndexReachable = 0;
+
+        for(int i = 0; i < nums.length; i++){
+
+            if(i > maxIndexReachable){
                 return false;
             }
 
-            max = Math.max(max, i + nums[i]);
+            maxIndexReachable = Math.max(maxIndexReachable, i + nums[i]);
         }
 
-    return max >= nums.length - 1;
+        return maxIndexReachable >= nums.length - 1;
+        
     }
 }
