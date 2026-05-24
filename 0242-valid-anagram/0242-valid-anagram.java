824d1bf349cca50b9f3706e4cae3ca47cbcd21a1
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2026-05-24 14:01:55 -0400

    Time: 5 ms (62.54%), Space: 44.5 MB (66.78%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,43 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        /*
-            - check if t has
-                - same length as s
-                - same freq of all chars in s
-            
-            - Use a hashset/map. --> map to store frequency as well
-            - Traverse s and populate the hashmap
-            - Traverse t and check for each char
-                - if char exists in the map and has freq > 0
-                - if not, we cannot make anagram
-                - Else, anagram is possible till ith index
-
-            - By the end, i is at the last index => anagram is possible
-        */
-
         //Edge case
-        if(s.length() != t.length()){
-            return false;
+        if(s.length() != t.length()) return false;
+
+        //Initialize count array
+        int[] count = new int[26];
+
+        //Populate the count array
+        for(int i = 0; i < s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
 
-        int[] freq = new int[26];
-
-        //Populate the hashmap
-        for(char c : s.toCharArray()){
-            freq[c - 'a'] ++;
-        }
-
-        //Check for anagram in t
-        for(char c: t.toCharArray()){
-            
-            if(freq[c - 'a'] <= 0){
-                return false;
-            }
-
-            freq[c - 'a']--;
+        for(int i : count){
+            if(i != 0) return false;
         }
 
         return true;
+        
     }
 }
