cf370edbe1a1ee5b201b35509ec22b1c97b33d43
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2026-05-24 14:20:21 -0400

    Time: 2 ms (99.4%), Space: 47.4 MB (11.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,43 @@
+
+        /*
+            - We need to check if there is a pair of ints in nums s.t. their sum = target
+            - return a int pair of indices (array)
+            - check for sums
+            - array traversal, sorting, find one working case,
+            - exaclty one solution
+            - min input len = 2
+
+                a
+            - 2,7,11,15]
+                   b
+
+            sum = 18
+            t = 18
+
+            - nested loops
+            - sorting - space efficient
+            - hash set - time efficient with space trade off
+
+        */
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
+
+        //Hashmap to store occurence of every input number
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < nums.length; i++){
+        //Populate the map
+        for(int i=0; i < nums.length; i++){
             int n = nums[i];
-
             if(map.containsKey(target - n)){
                 return new int[]{i, map.get(target - n)};
             }
 
             map.put(n, i);
         }
+        
 
         return new int[]{};
-        
     }
 }
