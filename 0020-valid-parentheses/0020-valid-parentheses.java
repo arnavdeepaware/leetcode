c5ebed35f82e5543b435f052139234ca4ce885e6
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2026-05-30 13:52:11 -0400

    Time: 4 ms (39.73%), Space: 43 MB (84.14%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,54 +1,43 @@
 class Solution {
-    /*
-        - String be empty?
-    
-        - Use stack to keep order of parentheses
-        - Use hashmap to pair opening and closing types
-        - traverse the input
-        - if opening bracket, add to stack
-        - if not, i.e. closing bracket:
-                - check if stack is not empty (shouldn't be to be valid)
-                - it should be of same type as top element of stack to be valid
-                - if valid, pop the top element, continue
-                - else not valid, return false
-                
-        - At the end, check if stack is empty
-        - If yes, we have a valid input, return true
-        - else return false
-    */
-
-    
     public boolean isValid(String s) {
-        
-        //Hashmap
+
+        /*
+            
+            Have a stack
+            add all opening brackets
+            for any closing bracket,
+                - not good, if stack is empty
+                - not good, if top opening bracket should match
+                - good if opening matches
+
+            return true if st is empty
+        */
+
+        //HashMap and stack
         HashMap<Character, Character> map = new HashMap<>();
         map.put(')', '(');
         map.put(']', '[');
         map.put('}', '{');
 
-        //Stack
         Stack<Character> st = new Stack<>();
 
-        //Traversal
-        for(char c: s.toCharArray()){
-            
-            //Check for opening bracket
+        //Traversal of Input String
+        for(char c : s.toCharArray()){
+            //if opening bracket add to stack
             if(!map.containsKey(c)){
-                st.add(c);
-                
-            }else{
-                if(st.empty() || map.get(c) != st.pop()){
+                st.push(c);
+            }
+            //if closing bracket
+            else{
+                if(st.isEmpty() || st.peek() != map.get(c)){
                     return false;
                 }
+
+                st.pop(); 
             }
         }
 
-        if(!st.empty()){
-            return false;
-        }
-
-        return true;
+        return st.isEmpty();
         
     }
 }
-
