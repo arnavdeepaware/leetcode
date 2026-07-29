5fb4b62c38438991a0e1d4b75e981908b5a9ec50
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2026-07-29 14:02:57 -0400

    Time: 18 ms (58.6%), Space: 108.3 MB (17.07%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-
+        
+        //Hashset
         HashSet<Integer> set = new HashSet<>();
 
-        for(int n : nums){
+        //Loop
+        for(int n: nums){
             if(set.contains(n)) return true;
             set.add(n);
         }
@@ -11,3 +13,17 @@ class Solution {
         return false;
     }
 }
+
+
+/*
+    - int array as input 
+    - t/f as output
+    - check if any value reoccurres even once 
+
+    - can array be empty -- no, at least 1 
+    - can there be multiple duplicates, if so can we return any? -> yes and yes
+
+    - nested for loops
+    - sort the array
+    - hashset to store numbers and check if it occurred before 
+*/
