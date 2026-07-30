54c16cd3f805c8c55f616407e4705bf84912e7e7
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2026-07-30 00:51:14 -0400

    Time: 5 ms (61.69%), Space: 44.3 MB (74.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,23 +1,48 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
-        //Edge case
+        
+        //Edge Case
         if(s.length() != t.length()) return false;
 
-        //Initialize count array
-        int[] count = new int[26];
+        //int[] of size 26
+        int[] chars = new int[26];
 
-        //Populate the count array
+        //Traver s and t
         for(int i = 0; i < s.length(); i++){
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
+            
+            char c;
+            //Increment for s
+            c = s.charAt(i);
+            chars[c - 'a']++;
+
+            //Decrement for t
+            c = t.charAt(i);
+            chars[c - 'a']--;
         }
 
-        for(int i : count){
-            if(i != 0) return false;
+        //Traversal to check for anagram
+        for(int i = 0; i < chars.length; i++){
+            if(chars[i] != 0) return false;
         }
 
         return true;
-        
     }
 }
+
+/* 
+    - return t/f
+    - same strings -> true
+    - len of s and t are same -> can be different
+    - can either of the string be empty
+    - lowercase chars only? -> yes
+
+
+    - use int[] of size 26
+    - increment the count of chars on the int[] for s
+    - decrement the count for t
+    - loop again and check if all values in int[] are 0,
+                - if yes, return true;
+                - i no, return false;
+
+    - edge case(s); - t and s have diff len -> return False
+*/
