30bb7facf2d6401de5a10bf9e886b884385c0981
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2026-08-06 20:35:12 -0400

    Time: 3 ms (87.5%), Space: 52.1 MB (69%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,22 +1,23 @@
 class Solution {
     public int numIslands(char[][] grid) {
 
-        int res = 0;
+        int numOfIslands = 0;
+        
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == '1'){
-                    res ++;
-                    helper(grid, i ,j);
+                    numOfIslands++;
+                    helper(grid, i, j);
                 }
             }
         }
 
-        return res;
+        return numOfIslands;
     }
 
-    //Helper function - dfs or bfs?
-    public void helper(char[][] grid, int i, int j){
-        //Base Case
+    //helper
+    public void helper(char[][]grid, int i, int j){
+        //Edge case
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0'){
             return;
         }
@@ -24,14 +25,16 @@ class Solution {
         grid[i][j] = '0';
 
         //Recursive Step
-        
-        //left
+
+        //Left
         helper(grid, i-1, j);
-        //Right
-        helper(grid, i+1, j);
         //Up
         helper(grid, i, j-1);
+        //Right
+        helper(grid, i+1, j);
         //Down
-        helper(grid, i, j+1); 
+        helper(grid, i, j+1);
+
+        return;
     }
 }
