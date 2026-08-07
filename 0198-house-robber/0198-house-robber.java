a2a88ec49ebd369eee24ed67b5f4668263921dd6
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2026-08-06 20:05:29 -0400

    Time: 0 ms (100%), Space: 43 MB (19.35%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,29 +1,20 @@
 class Solution {
     public int rob(int[] nums) {
 
-        /*
-            - base case first two houses: choose the one with more money
-
-            - from third house: 2 choices: we will optimize every iteration
-
-            -  we can either choose curr house 'i' + 'i-2' 
-            -  or we can only choose 'i-1'
-
-            [2,7,11,11,12]
-        */
-
         int n = nums.length;
 
-        //Edge Cases
+        //Edge case
         if(n == 1) return nums[0];
         if(n == 2) return Math.max(nums[0], nums[1]);
-        
-        nums[n-2] = Math.max(nums[n-2], nums[n-1]);
 
-        for(int i = n-3; i >= 0; i--){
-            nums[i] = Math.max(nums[i+2] + nums[i], nums[i+1]);
+        //Best choice for first two houses
+        nums[1] = Math.max(nums[0], nums[1]);
+
+        //Traversal
+        for(int i = 2; i < n; i++){
+            nums[i] = Math.max(nums[i] + nums[i-2], nums[i-1]);
         }
 
-        return nums[0];
+        return nums[n-1];
     }
 }
