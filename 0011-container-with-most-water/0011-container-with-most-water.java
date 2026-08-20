d8c50285f23237ece1fa2e72a462aae0777de00c
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2026-08-20 08:53:11 -0400

    Time: 5 ms (82.93%), Space: 77.4 MB (49.65%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,19 +1,26 @@
 class Solution {
     public int maxArea(int[] height) {
+
+        //Two pointers
+        int l = 0;
+        int r = height.length - 1;
+
+        //Max Area
         int max = 0;
-        int i = 0, j = height.length-1;
-        while (i < j) {
-            int cur = 0;
-            int left = height[i], right = height[j];
-            if (left <= right) {
-                cur = (j-i) * left;
-                while (i < j && height[i] <= left) i++;
-            } else {
-                cur = (j-i) * right;
-                while (i < j && height[j] <= right) j--;
-            }
-            if (max < cur) max = cur;
+
+        //Traversal
+        while(l < r){
+            //Check area vs max
+            int width = r - l;
+            int length = Math.min(height[l], height[r]);
+
+            max = Math.max(max, width * length);
+
+            //Move smaller length pointer
+            if(height[l] < height[r]) l++;
+            else r--;
         }
+
         return max;
     }
 }
