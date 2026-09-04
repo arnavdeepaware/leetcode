1cd8443ae82f65422d532d2aade49263b6b10af4
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2026-09-04 17:21:26 -0400

    Time: 1 ms (99.89%), Space: 55.8 MB (31.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,43 +3,59 @@ class Solution {
         
         /*
             U - Understand
-            - Min. length of input array?
-            - Does the answer always exist?
+            - Min. length of input array? -> 1
+            - Does the answer always exist? -> yes
+            - can numbers be negative or zero? -> no
+            - can be equal to n/2 or strictly greater than n/2? -> strictly greater
 
-            -> 5 3 5 5 3
+            -> 3 5 5 5 3 5 5 4
 
-            - Count variable and tempX
-            - iterate through nums
-            - assign tempX as nums[0]
-            - At each iteraton
-                    - if num[i] == tempX
-                        - increase count
-                    - else
-                        - decrease count
-                        - if count == 0
-                        - tempX = nums[i] -- check for last element 
+            M - Match
 
-            - return tempX
+             - Brute force. - nested loops
+             - Hash Map - map the count for every number, return the num when it hits n/2 + 1
+             - Sorting - return n/2
+
+             - boyer - moore voting algorithm
+
+             P - Plan
+
+             - We know there is one number, let's call it X that occurs atleast more than once than all other numbers combined. Let's call all other numbers combined together as Y (Y can be single or multiple numbers)
+
+             - X category will always occur more than Y category. Y will be completely canceled out.
+
+             - For example, we have 6 occurences of X and 5 of Y category, if we count +- 1 in an efficient way, we will be left with leftovers of X
+
+             
+
+             - declare ct and tempX
+             - Traverse all of nums (input)
+             - At each occurence,
+
+                - if ct == 0, update tempX to be that num
+                - if num == tempX, 
+                        ct increments, 
+                        else decrments
+
+            - after loop, return tempX
+
+            
         */
 
         int ct = 0;
-        Integer temp = null;
+        int tempX = 0;
 
-        for(int n: nums){
-            if(ct == 0) temp = n;
+        //Loop starts
+        for(int num : nums){
+            // update tempX to be num
+            if(ct == 0) tempX = num;
 
-            ct += (n == temp) ? 1: -1;
+            //increment or decrement num acc to condition
+            ct += (num == tempX) ? 1 : -1; 
         }
+        //Loop ends
 
-        return temp;
+
+        return tempX;
     }
 }
-
-/*
-             m
-    - [1,2,2,2,2,6,9]
-
-    
-                m
-    - [[1,1,1,2,2,2,2,2]]
-*/
