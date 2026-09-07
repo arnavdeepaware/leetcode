af6f4ef1e4e3ffc13c6f27cb957c03b88718c547
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2026-09-07 18:36:38 -0400

    Time: 0 ms (100%), Space: 43.2 MB (15.27%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,41 +1,72 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ']') {
-                List<Character> decodedString = new ArrayList<>();
-                // get the encoded string
-                while (stack.peek() != '[') {
-                    decodedString.add(stack.pop());
-                }
-                // pop [ from the stack
-                stack.pop();
-                int base = 1;
-                int k = 0;
-                // get the number k
-                while (!stack.isEmpty() && Character.isDigit(stack.peek())) {
-                    k = k + (stack.pop() - '0') * base;
-                    base *= 10;
-                }
-                // decode k[decodedString], by pushing decodedString k times into stack
-                while (k != 0) {
-                    for (int j = decodedString.size() - 1; j >= 0; j--) {
-                        stack.push(decodedString.get(j));
-                    }
-                    k--;
-                }
+        
+        //Stacks
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+
+        //curr variables
+        int k = 0;
+        StringBuilder currString = new StringBuilder();
+
+        //Traversal
+        for(char ch : s.toCharArray()){
+
+            //Case 1: int - add to k
+            if(Character.isDigit(ch)){
+                //Mutliply k by 10 and then add lastest int as last digit
+                k = k * 10 + (ch - '0');
+            } 
+            //Case 2: opening bracket, add to stack and reset curr variables
+            else if(ch == '['){
+                //Add to stacks
+                countStack.push(k);
+                stringStack.push(currString);
+                //Reset
+                k = 0;
+                currString = new StringBuilder();
             }
-            // push the current character to stack
-            else {
-                stack.push(s.charAt(i));
+            //Case 3: closing bracket, append currString k times to popped decodedString
+            else if(ch == ']'){
+                //Pop Decoded String
+                StringBuilder decodedString = stringStack.pop();
+                //Append currString k times
+                for(int i = countStack.pop(); i > 0; i--){
+                    decodedString.append(currString);
+                }
+                currString = decodedString;
             }
-        }      
-        // get the result from stack
-        char[] result = new char[stack.size()];
-        for (int i = result.length - 1; i >= 0; i--) {
-            result[i] = stack.pop();
+            //Case 4: char - add to currString
+            else{
+                currString.append(ch);
+            }
         }
-        return new String(result);
+
+        return currString.toString();
     }
 }
 
+
+/*
+    - need to generate a string from repeating substrings
+    - ints are for #k, chars are for substring
+    - two stacks? -> one more numbers, one for substrings
+
+    - push every int onto the stack,
+    - after the int, a opening bracket will follow
+    - add following ints as needed
+    - for everysubstring, add to second stack
+    - at every closing bracket
+            - if stack.size == 1, add computed string to output.
+            - else 
+                    - pop and compute string - call it 'curr'
+                    - pop again (prev), compute prev + curr = new
+                    - push new to stack
+    - repeat until end of string
+
+    countStack - store ints #k
+    stringStack - store substring
+    k - tracks current k
+    currString - tracks current string (will append to this)
+    decodedString - getds appended to currString after computing k times
+*/
