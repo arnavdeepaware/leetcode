616474b4d3251dc6aa38f1c9c7843d6ed685fd15
Arnav Deepaware <140032603+arnavdeepaware@users.noreply.github.com>
2026-09-08 03:43:35 -0400

    Time: 1 ms (56.89%), Space: 47.4 MB (95.27%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,19 +1,30 @@
-public class Solution {
+class Solution {
     public int trap(int[] height) {
+        
+        //Total water
+        int water = 0;
+
+        //Moving Pointers
         int left = 0, right = height.length - 1;
-        int ans = 0;
-        int left_max = 0, right_max = 0;
-        while (left < right) {
-            if (height[left] < height[right]) {
-                left_max = Math.max(left_max, height[left]);
-                ans += left_max - height[left];
-                ++left;
-            } else {
-                right_max = Math.max(right_max, height[right]);
-                ans += right_max - height[right];
-                --right;
+        
+        //Max pointers
+        int leftMax = height[left], rightMax = height[right];
+
+        while(left < right){
+            //Compare which side is smaller
+            if(height[left] < height[right]){
+                //Update left side and add to water
+                leftMax = Math.max(leftMax, height[left]);
+                water += leftMax - height[left];
+                left++;
+            }else{
+                //Update right side and add to water
+                rightMax = Math.max(rightMax, height[right]);
+                water += rightMax - height[right];
+                right--;
             }
         }
-        return ans;
+
+        return water;
     }
 }
